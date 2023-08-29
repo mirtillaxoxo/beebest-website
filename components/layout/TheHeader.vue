2a5ad28d702c089c1ca3f02ec088425bad758395
arb85 <arb85@users.noreply.github.com>
2023-08-29 19:49:20 +0200

    feat: :lipstick: Updated header with bee logo

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -8,22 +8,35 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <header class="mt-5 mb-1 px-5">
+  <header class="mt-5 mb-1 px-5 relative">
     <div
       class="flex justify-between items-center border-b"
       style="border-color: var(--light-grey)"
     >
-      <NuxtLink to="/" class="font-serif font-black text-3xl">BeeBest</NuxtLink>
+      <NuxtLink to="/" class="font-serif font-black text-3xl">
+        <h1>BeeBest</h1>
+      </NuxtLink>
 
       <button
         type="button"
         @click="modalDialog?.showModal()"
         class="px-10 border-l font-uppercase font-black"
-        style="border-color: var(--light-grey); color: var(--light-grey)"
+        style="
+          border-color: var(--light-grey, #d0d0d0);
+          color: var(--light-grey, #d0d0d0);
+        "
       >
         Menu
       </button>
     </div>
+    <figure class="absolute w-full flex justify-center h-7 top-12.8 left-0">
+      <img
+        src="~/assets/graphics/bee-best_bee-glyph.svg"
+        alt="Bee Best Bee Glyph"
+        class="h-full px-1 bg-white"
+        style="background-color: var(--background-white, #f1f1f1)"
+      />
+    </figure>
   </header>
 </template>
 
