d5dc60150886b8dec729c8eb59346524b1538330
arb85 <arb85@users.noreply.github.com>
2023-11-09 09:50:57 +0100

    feat: :heavy_plus_sign: Added localization logic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,33 @@
 export default defineNuxtConfig({
   modules: [
     '@unocss/nuxt',
+    '@nuxtjs/i18n',
     '@vueuse/nuxt',
+
     '@nuxtjs/html-validator',
     'nuxt-typed-router',
   ],
 
   css: [
-    '~/assets/css/main.css',
     '~/assets/css/style.css',
     '~/assets/css/fonts.css',
     '~/assets/css/shadows.css',
   ],
+
+  // CONFIG
+  runtimeConfig: {},
+
+  // LOCALIZATION
+  i18n: {
+    lazy: true,
+    langDir: 'locales',
+    locales: [
+      { code: 'en', file: 'en-US.ts' },
+      { code: 'it', file: 'it-IT.ts' },
+    ],
+    defaultLocale: 'en',
+    experimental: { jsTsFormatResource: true }, // to enable .ts locales file
+  },
 
   devtools: { enabled: true },
 
