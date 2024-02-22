1a6b4518ee12ea90fb84dcce3d589d71025566ba
arb85 <arb85@users.noreply.github.com>
2024-02-23 00:23:20 +0100

    refactor: :lipstick: Updated uno configuration and css styles

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,3 +1,5 @@
+import { readFile } from 'fs/promises'
+
 import {
   defineConfig,
   presetUno,
@@ -5,12 +7,25 @@ import {
   transformerDirectives,
 } from 'unocss'
 
+// add icon here to load dynamic icon
+import { icons } from './composables/useIcons'
+
+// https://github.com/unocss/unocss/tree/main/packages/nuxt
 export default defineConfig({
   presets: [
     // https://unocss.dev/presets/uno
     presetUno({}),
     // https://unocss.dev/presets/icons
-    presetIcons({ collections: {} }),
+    presetIcons({
+      collections: {
+        'bee-best': {
+          // https://unocss.dev/presets/icons#node-js
+          'bee-glyph': () =>
+            readFile('./assets/graphics/logos/bee-best_bee-glyph.svg', 'utf-8'),
+        },
+      },
+      prefix: 'i-',
+    }),
   ],
 
   transformers: [
@@ -30,5 +45,11 @@ export default defineConfig({
     ['font-body', { 'font-family': 'Vollkorn' }],
     ['font-outline', { 'font-family': 'Ostrich Sans' }],
     ['font-display', { 'font-family': 'Blackout' }],
+  ],
+
+  // https://unocss.dev/guide/extracting
+  safelist: [
+    //   // to use dynamic icons included in useIcon composable
+    ...Object.values(icons),
   ],
 })
