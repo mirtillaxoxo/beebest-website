2603c525ee3e75b056558906d74e2dfe8361038c
arb85 <arb85@users.noreply.github.com>
2024-04-02 02:06:24 +0200

    feat: :sparkles: Added and improved new pages

--- composables/usePages.ts
+++ composables/usePages.ts
@@ -2,6 +2,7 @@ type WebsitePage = { name: string; description?: string; url: string }
 
 export const usePages = (): Array<WebsitePage> => [
   { name: 'home', url: '/' },
+  { name: 'services', url: '/services' },
   { name: 'projects', url: '/projects' },
   { name: 'about', url: '/about' },
   { name: 'contacts', url: '/contacts' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,98 @@
 <template>
-  <article>
-    <!--   <LayoutCardContainer>
-     <LayoutCardArticle
-        tag="@work"
-        class="w-full sm:w-64 min-h-108 sm:h-108 overflow-hidden grow-0"
-      >
-        <video
-          autoplay
-          muted
-          loop
-          playsinline
-          class="absolute -z-10 -mx-4 bottom-0"
-        >
-          <source src="/video/bee-pollination.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+  <LayoutThePage>
+    <!-- Bee Animation -->
+    <section class="page-section-margins">
+      <GraphicsBeeAnimation class="-m-5 -mt-10" />
+    </section>
 
-        <div class="absolute left-3 bottom-5 text-shadow-sm">
-          <p class="text-white">
-            Bees are 'ensembles'<br />
-            and not individuals.
-            <br />
-            Outside the community,<br />
-            they cannot live. <br /><span
-              class="pt-3 pl-3 inline-block text-sm font-black"
-              >Mario Rigoni Stern</span
-            >
-          </p>
-        </div>
-      </LayoutCardArticle>
-      <LayoutCardArticle tag="#approach" class="flex-grow">
-        <template v-slot:title>
-          <h2>The Methodological Approach</h2>
-        </template>
-        <p>
-          <strong>Bees</strong> are known and appreciated for being
-          <strong>tireless workers</strong> and
-          <strong>precise designers</strong>: they create perfectly
-          hexagonal-shaped cells that make up the combs.
-        </p>
-        <p>
-          The choice is not random: the hexagonal shape allows the greatest
-          number of cells to be built in the smallest available space.
-        </p>
-        <p>
-          <strong>We create projects</strong>
-          with passion and commitment and with great attention to
-          <strong>aesthetics</strong> and <strong>functional</strong> content.
-        </p>
-      </LayoutCardArticle>
-    </LayoutCardContainer> -->
-  </article>
+    <section class="-mt-15 text-xl page-section-margins">
+      <p>
+        <span class="font-bold text-3xl">We are like bees,</span> <br />
+        known and appreciated <br />
+        for being tireless workers <br />
+        and precise designers.
+      </p>
+
+      <div class="flex justify-center py-3">
+        <span
+          class="i-bee-best-bee-glyph-fill text-bb-honey inline-block text-3xl"
+        ></span>
+      </div>
+
+      <p class="text-right">
+        <span class="font-bold text-3xl">We create projects</span> <br />
+        with passion and commitment,<br />
+        with great attention to aesthetic <br />
+        and functional content.
+      </p>
+    </section>
+
+    <LayoutPageSection section-title="What we love">
+      <div class="mt-8 md:mx-10 flex flex-wrap justify-center gap-10">
+        <figure
+          class="category-figure color-middle-gray md:w-[55%]! lg:w-[45%]!"
+        >
+          <NuxtImg
+            src="images/categories/adv_horm-2022_01.jpg"
+            alt="Design Category Image"
+          />
+          <figcaption>
+            <h3>design & tech</h3>
+          </figcaption>
+        </figure>
+
+        <figure class="category-figure color-middle-gray w-[80%] md:w-[30%]!">
+          <NuxtImg
+            src="images/categories/adv_marina-babini-2001_01.jpg"
+            alt="Design Category Image"
+          />
+          <figcaption>
+            <h3>fashion</h3>
+          </figcaption>
+        </figure>
+
+        <figure class="category-figure color-middle-gray md:-mt-10">
+          <NuxtImg
+            src="images/categories/projects_chocolove-2023_01.jpg"
+            alt="Design Category Image"
+          />
+          <figcaption>
+            <h3>food</h3>
+          </figcaption>
+        </figure>
+      </div>
+    </LayoutPageSection>
+
+    <LayoutPageSection section-title="What we do"></LayoutPageSection>
+
+    <LayoutPageSection section-title="Who we are"></LayoutPageSection>
+  </LayoutThePage>
 </template>
+
+<style scoped>
+.category-figure {
+  @apply md:w-[45%] lg:w-[30%];
+}
+.category-figure :deep(img) {
+  @apply w-full md:shadow-xl;
+}
+.category-figure figcaption {
+  @apply flex justify-end;
+
+  /* background: linear-gradient(70deg, #f1f1f1 50%, #fda100 90%);
+  background: linear-gradient(
+    70deg in oklch,
+    oklch(95.81% 0 0) 60%,
+    oklch(78.33% 0.17 69.28) 90%
+  ); */
+}
+.category-figure figcaption h3 {
+  @apply px-5 pt-1 -mt-2 mr-2
+  text-3xl font-bold text-white;
+
+  background-color: #fda100;
+  background-color: oklch(78.33% 0.17 69.28);
+
+  text-shadow: var(--shadow-elevation-medium);
+}
+</style>
