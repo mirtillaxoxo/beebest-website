7476d8c14ebabb698d9d4c0d912014f4bdd3a24f
arb85 <arb85@users.noreply.github.com>
2024-04-02 02:03:58 +0200

    feat: :mag: Added head logic and favicon components

---
+++ .config/websiteHead.ts
@@ -0,0 +1,51 @@
+import type { AppHeadMetaObject } from '@nuxt/schema'
+
+import { useThemeColors } from '../composables/useThemeColors'
+const { honey, backgroundWhite } = useThemeColors()
+
+// FAVICONS
+// TO invalidate favicon Chache
+const cacheRefreshVersion = 1 // #TODO: use something like YYYY-MM-DD from new Date() to automatic refresh chache
+
+export const faviconLinks: AppHeadMetaObject['link'] = [
+  // FAVICONS: https://realfavicongenerator.net
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: `/apple-touch-icon.png?v=${cacheRefreshVersion}`,
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: `/favicon-32x32.png?v=${cacheRefreshVersion}`,
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: `/favicon-16x16.png?v=${cacheRefreshVersion}`,
+  },
+  { rel: 'manifest', href: '/site.webmanifest' },
+  {
+    rel: 'mask-icon',
+    href: `/safari-pinned-tab.svg?v=${cacheRefreshVersion}`,
+    color: honey?.hex,
+  },
+  {
+    rel: 'shortcut icon',
+    href: `/favicon.ico?v=${cacheRefreshVersion}`,
+  },
+]
+
+export const appColorsMeta: AppHeadMetaObject['meta'] = [
+  {
+    name: 'msapplication-TileColor',
+    content: '#2b5797',
+    // #TODO: extract this into a variable
+  },
+  {
+    name: 'theme-color',
+    content: backgroundWhite?.hex,
+  },
+]
