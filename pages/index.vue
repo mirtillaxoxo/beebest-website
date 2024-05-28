835899cfd7ea8914303e0138d2d93b93ce3c60e7
arb85 <arb85@users.noreply.github.com>
2024-05-28 13:33:13 +0200

    chore: :construction: Improved homepage

--- pages/index.vue
+++ pages/index.vue
@@ -43,24 +43,27 @@
       </div>
     </section>
 
+    <!-- what we love -->
     <section>
-      <h2 class="flex items-center gap-2.5">
-        <span class="i-arcticons-hearth text-xl"></span>
+      <h2 class="section-title">
+        <span class="i-arcticons-hearth"></span>
 
-        <span class="uppercase">What we love: </span>
+        <span>What we love: </span>
       </h2>
-      <ul class="what-we-love-container">
-        <li>
-          <figure>
-            <div>
-              <NuxtImg
-                src="/images/categories/adv_horm-2022_01.jpg"
-                alt="Design"
-              />
-            </div>
-            <figcaption>Design</figcaption>
-          </figure>
-        </li>
+      <nav
+        aria-label="macro-categories-navigation"
+        class="what-we-love-container"
+      >
+        <figure>
+          <div>
+            <NuxtImg
+              src="/images/categories/projects_showroom-novacolor-24_materia-viva_conchiglia.jpg"
+              alt="Design"
+            />
+          </div>
+          <figcaption>Design</figcaption>
+        </figure>
+
         <figure>
           <div>
             <NuxtImg
@@ -70,35 +73,81 @@
           </div>
           <figcaption>Fashion</figcaption>
         </figure>
-        <li>
-          <figure>
-            <div>
-              <NuxtImg
-                src="/images/categories/projects_chocolove-2023_01.jpg"
-                alt="Food"
-              />
-            </div>
-            <figcaption>Food</figcaption>
-          </figure>
-        </li>
 
-        <li class="md:hidden">
-          <div class="">
-            <div class="pb-[100%] relative">
-              <div
-                class="w-full h-full absolute top-0 flex justify-center items-center"
-              >
-                <span
-                  class="i-bee-best-bee-glyph text-[9rem] text-bb-honey"
-                ></span>
-              </div>
-            </div>
-
-            <p>..Bees!</p>
+        <figure>
+          <div>
+            <NuxtImg
+              src="/images/categories/projects_chocolove-2023_01.jpg"
+              alt="Food"
+            />
           </div>
-        </li>
-      </ul>
+          <figcaption>Food</figcaption>
+        </figure>
+
+        <div class="md:hidden">
+          <div class="pb-[100%] relative">
+            <div
+              class="w-full h-full absolute top-0 flex justify-center items-center"
+            >
+              <span
+                class="i-bee-best-bee-glyph text-[9rem] text-bb-honey"
+              ></span>
+            </div>
+          </div>
+
+          <p>..Bees!</p>
+        </div>
+      </nav>
     </section>
+
+    <!-- our services -->
+    <section>
+      <div class="grid items-start grid-cols-1 md:grid-cols-3 gap-5 font-mono">
+        <h2 class="section-title">
+          <span class="i-arcticons-keyboard"></span>
+          <span>What we do: </span>
+        </h2>
+
+        <div class="bg-bb-lightgrey -mx-2.5 md:mx-0 md:aspect-square">
+          <div class="flex md:flex-col items-center md:justify-center gap-2.5">
+            <span
+              class="i-arcticons-vr-media-player inline-block ml-2.5 md:ml-0 animate-spin animate-duration-[3000ms] md:text-6xl"
+            ></span>
+            <span>Digital Projects</span>
+          </div>
+        </div>
+
+        <div class="services-container">
+          <h3>Integrated Communication</h3>
+          <ul>
+            <li>
+              <span></span>
+              <span>Press Office, PR and Digital PR</span>
+            </li>
+            <li>
+              <span></span>
+              <span>Social Media Marketing</span>
+            </li>
+            <li>
+              <span></span>
+              <span>Online/Offline communication tools design</span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="bg-bb-lightgrey h-full -mx-2.5 md:mx-0">
+          <div
+            class="flex md:flex-col items-center md:h-full md:justify-center gap-2.5"
+          >
+            <span
+              class="i-arcticons-vr-media-player inline-block ml-2.5 md:ml-0 animate-spin animate-duration-[3000ms] md:text-6xl"
+            ></span>
+            <span>Digital Projects</span>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <section>
       <div class="flex justify-center">
         <div class="relative">
@@ -128,7 +177,7 @@
   md:grid md:grid-cols-3 md:gap-5
   -mb-2.5;
 }
-.what-we-love-container li {
+.what-we-love-container > * {
   @apply mb-2.5;
 }
 
@@ -143,5 +192,15 @@
 }
 .what-we-love-container figcaption {
   @apply mt-1;
+}
+
+.services-container h3 {
+  @apply underline;
+}
+.services-container > ul > li {
+  @apply flex gap-1 pt-2;
+}
+.services-container > ul > li > span:first-of-type {
+  @apply i-ph:arrow-down-right text-bb-honey min-w-6;
 }
 </style>
